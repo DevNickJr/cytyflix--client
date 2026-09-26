8ae1050bdc38d3a2629854ce7217013a328f36a2
DevNickJr <nicholasduadei14@gmail.com>
2026-09-26 15:19:15 +0100

    update image

--- next.config.ts
+++ next.config.ts
@@ -10,32 +10,32 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/_next/image(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Resource-Policy',
-            value: 'cross-origin',
-          },
-        ],
-      },
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     // {
+  //     //   source: '/_next/image(.*)',
+  //     //   headers: [
+  //     //     {
+  //     //       key: 'Cross-Origin-Resource-Policy',
+  //     //       value: 'cross-origin',
+  //     //     },
+  //     //   ],
+  //     // },
+  //     // {
+  //     //   source: '/(.*)',
+  //     //   headers: [
+  //     //     {
+  //     //       key: 'Cross-Origin-Opener-Policy',
+  //     //       value: 'same-origin',
+  //     //     },
+  //     //     {
+  //     //       key: 'Cross-Origin-Embedder-Policy',
+  //     //       value: 'require-corp',
+  //     //     },
+  //     //   ],
+  //     // },
+  //   ];
+  // },
 };
 
 export default nextConfig;
