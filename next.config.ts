f9cd3e4ba099c50632856e37396989f1907a8a87
DevNickJr <nicholasduadei14@gmail.com>
2026-09-26 13:57:53 +0100

    fix next image

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '://googleapis.com',
+        hostname: 'firebasestorage.googleapis.com',
       },
     ],
   },
